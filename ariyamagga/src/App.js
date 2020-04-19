ed1e27afcae4008a72e5a040ed58025a4832e24c
Kavan Mannapperuma <47651784+kmannapperuma@users.noreply.github.com>
2020-04-19 16:52:25 +1000

    Add navbar

--- ariyamagga/src/App.js
+++ ariyamagga/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
-  return (
+  return (  
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Navbar />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
