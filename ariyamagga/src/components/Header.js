d0ac75b09720463766a0a3706ba903e6196e9604
Kavan Mannapperuma <47651784+kmannapperuma@users.noreply.github.com>
2020-05-15 21:10:23 +1000

    Add bootstrap

--- ariyamagga/src/components/Header.js
+++ ariyamagga/src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
     return (
-        <div>
-            <h3>අරියමග්ග සෙනසුණ</h3>
+        <div className="header">
+            <h3>අරියමග්ග සෙනසුන</h3>
             <h3>Ariyamagga Senasuna</h3>
         </div>
     );
--- ariyamagga/src/components/Navbar.js
+++ ariyamagga/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import Home from './Home';
 import About from './About';
 import Gallery from './Gallery';
+import Contact from './Contact';
 import {
     BrowserRouter as Router,
     Switch,
@@ -14,16 +15,18 @@ function Navbar() {
     return (
         <Router>
             <div>
-                <div className="Navbar">
+                <div className="navbar">
                     <a href="#home"><Link to="/">Home</Link></a>
                     <a href="#about"><Link to="/about">About</Link></a>
                     <a href="#gallery"><Link to="/gallery">Gallery</Link></a>
+                    <a href="#contact"><Link to="/contact">Contact</Link></a>
                 </div>
                 <Switch>
                     <Route exact path="/"><Home /></Route>
                     <Route exact path="/home"><Home /></Route>
                     <Route path="/about"><About /></Route>
                     <Route path="/gallery"><Gallery /></Route>
+                    <Route path="/contact"><Contact /></Route>
                     <Route exact path="*"><Home /></Route>
                 </Switch>
             </div>
