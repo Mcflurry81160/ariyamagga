159c3ee7f31ec5e8fb83ff8ceb951f1a7f190da2
Kavan Mannapperuma <47651784+kmannapperuma@users.noreply.github.com>
2020-05-15 21:18:58 +1000

    Add about section content

--- ariyamagga/src/components/Home.js
+++ ariyamagga/src/components/Home.js
@@ -3,8 +3,7 @@ import React from 'react';
 function Home() {
     return (
         <div className="container">
-            <h3>Welcome to Ariyamagga Senasuna</h3>
-            <p>Situated about 3KM from the Kurunegala clock tower/city centre, The place was set up in 2007 to provide temporary accommodation to those forest monks from various parts of the Kurunegala district seeking treatment at the medical facilities in Kurunegala.</p>
+           
         </div>
     );
 }
