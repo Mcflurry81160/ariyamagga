921975218cebce226417f4455272993a12c58787
kmannapperuma <kmannapperuma@machship.com>
2020-07-04 08:30:15 +1000

    Edit web title. Add gallery placeholder content

--- ariyamagga/src/App.js
+++ ariyamagga/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import About from './components/About';
-// import Gallery from './Gallery';
+import Gallery from './components/Gallery';
 import Contact from './components/Contact';
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
           <Switch>
             <Route exact path="/"><About /></Route>
             <Route path="/about"><About /></Route>
+            <Route path="/gallery"><Gallery /></Route>
             <Route path="/contact"><Contact /></Route>
           </Switch>
         </Router>
--- ariyamagga/src/components/Gallery.js
+++ ariyamagga/src/components/Gallery.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Gallery() {
     return (
         <div>
-            <p>Contact page content comes here</p>
+            <p>Gallery page content comes here</p>
         </div>
     );
 }
