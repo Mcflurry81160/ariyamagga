7612c2583253ffbf6bd4f7f06e15d8dff1da17df
kmannapperuma <kmannapperuma@machship.com>
2020-07-08 20:53:48 +1000

    Use image from Azure storage

--- ariyamagga/src/components/GalleryPage.js
+++ ariyamagga/src/components/GalleryPage.js
@@ -27,7 +27,7 @@ function GalleryPage() {
             <div class="col-md-4">
                 <div class="thumbnail">
                     <a href="/w3images/fjords.jpg">
-                        <img src="https://www.tripsavvy.com/thmb/ak3sgvCXavEvA-t3ao8Ui82AjM0=/950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wat-arun-temple-thailand-088091c0417e49c59e90e97597164457.jpg" alt="Fjords" style={{width:"100%"}}></img>
+                        <img src="https://ariyamaggasenasuna.blob.core.windows.net/imagegallery/download.jpg" alt="Fjords" style={{width:"100%"}}></img>
                         <div class="caption">
                             <p>Lorem ipsum...</p>
                         </div>
