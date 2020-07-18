abc8210e47db4abd123c12c1b96c9a45dc300a67
kmannapperuma <kmannapperuma@machship.com>
2020-07-18 17:22:24 +1000

    Fix gallery shrinking issue

--- ariyamagga/src/components/GalleryPage.js
+++ ariyamagga/src/components/GalleryPage.js
@@ -8,7 +8,7 @@ class GalleryPage extends React.Component {
 
         this.state = {
             loading: true,
-            images: this.props.photos
+            imageUrls: this.props.photos
         }
     }
 
@@ -16,30 +16,15 @@ class GalleryPage extends React.Component {
         let url = '';
         fetch(url)
             .then(response => response.json())
-            .then(images => this.setState({ images: images }))
+            .then(imageUrls => this.setState({ imageUrls: imageUrls }))
     }
 
     render() {
-
-        //TODO: implement the react photo gallery. See repository issues for details for the shrinking issue
-
 
         return (
             <div>
                 <Gallery photos={GetPhotos()} />
             </div>
-            // <div className="row">
-            //     <div class="col-md-4">
-            //         <div class="thumbnail">
-            //             <img src="https://www.tripsavvy.com/thmb/ak3sgvCXavEvA-t3ao8Ui82AjM0=/950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wat-arun-temple-thailand-088091c0417e49c59e90e97597164457.jpg" alt="Lights" style={{ width: "100%" }}></img>
-            //         </div>
-            //     </div>
-            //     <div class="col-md-4">
-            //         <div class="thumbnail">
-            //             <img src="https://ariyamaggasenasuna.blob.core.windows.net/imagegallery/download.jpg" alt="" style={{ width: "100%" }}></img>
-            //         </div>
-            //     </div>
-            // </div>
         );
     }
 }
@@ -47,15 +32,106 @@ class GalleryPage extends React.Component {
 function GetPhotos() {
     const photos = [
         {
-            src: 'http://example.com/example/img1.jpg',
+            src: 'https://ariyamaggasenasuna.blob.core.windows.net/imagegallery/download.jpg',
             width: 4,
             height: 3
         },
         {
-            src: 'http://example.com/example/img2.jpg',
+            src: 'https://www.tripsavvy.com/thmb/ak3sgvCXavEvA-t3ao8Ui82AjM0=/950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wat-arun-temple-thailand-088091c0417e49c59e90e97597164457.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://ariyamaggasenasuna.blob.core.windows.net/imagegallery/download.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://www.tripsavvy.com/thmb/ak3sgvCXavEvA-t3ao8Ui82AjM0=/950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wat-arun-temple-thailand-088091c0417e49c59e90e97597164457.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://ariyamaggasenasuna.blob.core.windows.net/imagegallery/download.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://ariyamaggasenasuna.blob.core.windows.net/imagegallery/download.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://www.tripsavvy.com/thmb/ak3sgvCXavEvA-t3ao8Ui82AjM0=/950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wat-arun-temple-thailand-088091c0417e49c59e90e97597164457.jpg',
             width: 1,
             height: 1
+        },
+        {
+            src: 'https://ariyamaggasenasuna.blob.core.windows.net/imagegallery/download.jpg',
+            width: 4,
+            height: 2
+        },
+        {
+            src: 'https://www.tripsavvy.com/thmb/ak3sgvCXavEvA-t3ao8Ui82AjM0=/950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wat-arun-temple-thailand-088091c0417e49c59e90e97597164457.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://ariyamaggasenasuna.blob.core.windows.net/imagegallery/download.jpg',
+            width: 4,
+            height: 3
+        }, 
+        {
+            src: 'https://ariyamaggasenasuna.blob.core.windows.net/imagegallery/download.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://www.tripsavvy.com/thmb/ak3sgvCXavEvA-t3ao8Ui82AjM0=/950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wat-arun-temple-thailand-088091c0417e49c59e90e97597164457.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://ariyamaggasenasuna.blob.core.windows.net/imagegallery/download.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://www.tripsavvy.com/thmb/ak3sgvCXavEvA-t3ao8Ui82AjM0=/950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wat-arun-temple-thailand-088091c0417e49c59e90e97597164457.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://ariyamaggasenasuna.blob.core.windows.net/imagegallery/download.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://ariyamaggasenasuna.blob.core.windows.net/imagegallery/download.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://www.tripsavvy.com/thmb/ak3sgvCXavEvA-t3ao8Ui82AjM0=/950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wat-arun-temple-thailand-088091c0417e49c59e90e97597164457.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://ariyamaggasenasuna.blob.core.windows.net/imagegallery/download.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://www.tripsavvy.com/thmb/ak3sgvCXavEvA-t3ao8Ui82AjM0=/950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wat-arun-temple-thailand-088091c0417e49c59e90e97597164457.jpg',
+            width: 4,
+            height: 3
+        },
+        {
+            src: 'https://ariyamaggasenasuna.blob.core.windows.net/imagegallery/download.jpg',
+            width: 4,
+            height: 3
         }
+
     ];
 
     return photos;
