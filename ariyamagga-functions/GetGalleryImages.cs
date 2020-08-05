10386bbf763605d49833a7cb45fb4d201e39cf58
kmannapperuma <kmannapperuma@machship.com>
2020-08-06 08:00:25 +1000

    [WIP] Start getting the list of blobs

--- ariyamagga-functions/GetGalleryImages.cs
+++ ariyamagga-functions/GetGalleryImages.cs
@@ -7,6 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace Ariyamagga.GetGalleryImages
 {
@@ -28,6 +30,30 @@ namespace Ariyamagga.GetGalleryImages
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+
+            //follow best practices when getting connection strings
+            var connectionString = "";
+            CloudStorageAccount backupStorageAccount = CloudStorageAccount.Parse(connectionString);
+
+            var backupBlobClient = backupStorageAccount.CreateCloudBlobClient();
+            var backupContainer = backupBlobClient.GetContainerReference("CONTAINER");
+
+            BlobContinuationToken blobContinuationToken = null;
+            var blobSegment = await backupContainer.ListBlobsSegmentedAsync( 
+                prefix: null,
+                useFlatBlobListing: true, 
+                blobListingDetails: BlobListingDetails.None,
+                maxResults: null,
+                currentToken: blobContinuationToken,
+                options: null,
+                operationContext: null);
+
+            foreach (var blob in blobSegment.Results)
+            {
+                //get the url of each blob here
+
+            }
 
             return new OkObjectResult(responseMessage);
         }
